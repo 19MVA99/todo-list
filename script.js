fe13ef402bd8bd612010546ef98a7fce3cbe8782
19MVA99 <macylevich.vl@gmail.com>
2025-01-13 12:26:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,25 +9,22 @@ const deleteButton = document.getElementById("deleteButton");
 
 // Initialize 
 
-document.addEventListener("DOMContentLoaded", 
-function ( ) {
+document.addEventListener("DOMContentLoaded", function ( ) {
         addButton.addEventListener("click", addTask);
-        todoInput.addEventListener('keydown', function (event) {
+        todoInput.addEventListener("keydown", function (event) {
             if (event.key === "Enter") {
                 event.preventDefault();
                 addTask();
             }
         });
-        deleteButton.addEventListener("click", deleteAllTasks());
+        deleteButton.addEventListener("click", deleteAllTasks);
         displayTasks();
 });
 
 function addTask() {
     const newTask = todoInput.value.trim();
-    if (newTask !=="") {
-        todo.push({
-            text: newTask, disabled:false,  
-        });
+    if (newTask !== "") {
+        todo.push({text: newTask, disabled: false});
         saveToLocalStorage();
         todoInput.value = "";
         displayTasks();
@@ -59,7 +56,7 @@ function editTask(index) {
     const inputElement = document.createElement("input"); 
 
     inputElement.value = existingText;
-    todoInput.replaceWith(inputElement);
+    todoItem.replaceWith(inputElement);
     inputElement.focus();
 
     inputElement.addEventListener("blur", function () {
